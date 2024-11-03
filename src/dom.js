d361f4be1b51202145ea466af9b461ebf62b4bf9
Ifham-Hashir <ifham09052003@gmail.com>
2024-11-03 08:42:12 +0530

    Add todo cards in dom

--- src/dom.js
+++ src/dom.js
@@ -3,8 +3,10 @@ logic();
 import { projects } from "./logic.js";
 export default function() {
   const sideBar = document.querySelector(".sidebar");
+  const todoContainer = document.querySelector(".todo-container");
   const projectList = document.createElement("ul");
   projectList.classList.add("project-list");
+
   for(let i = 0; i < projects.length; i++){
     let listItem = document.createElement("li");
     let link = document.createElement("a");
@@ -12,6 +14,45 @@ export default function() {
     link.href = "#";
     projectList.appendChild(listItem);
     listItem.appendChild(link);
+
+    link.addEventListener("click", () => {
+      while (todoContainer.firstChild) {
+        todoContainer.removeChild(todoContainer.firstChild);
+      }
+      for(let j = 0; j < projects[i].todoList.length; j++){
+        if(!projects[i].todoList[j]){
+          return;
+        }
+        else{
+          const todoCard = document.createElement("div");
+          todoCard.classList.add("todo-card");
+          todoContainer.appendChild(todoCard);
+          todoCard.dataset.index = j;
+  
+          const todoTitle = document.createElement("div");
+          todoCard.appendChild(todoTitle);
+          todoTitle.textContent = `${projects[i].todoList[j].title}`;
+  
+          const todoDueDate = document.createElement("div");
+          todoCard.appendChild(todoDueDate);
+          todoDueDate.textContent = `${projects[i].todoList[j].dueDate}`;
+  
+          const todoPriority = document.createElement("div");
+          todoCard.appendChild(todoPriority);
+          todoPriority.textContent = `${projects[i].todoList[j].priority}`;
+  
+          const removeBtn = document.createElement("button");
+          todoCard.appendChild(removeBtn);
+          removeBtn.textContent = "X";
+  
+          removeBtn.addEventListener("click", () => {
+            delete projects[i].todoList[j];
+            todoCard.remove();
+          });
+        }
+        //alert(projects[i].todoList[j].title);
+      }
+    });
   }
   sideBar.appendChild(projectList);
 }
--- src/logic.js
+++ src/logic.js
@@ -8,16 +8,15 @@ export default function() {
   const defaultTodo = new Project("defaultTodo", []);
   const arabic = new Project("arabic", []);
 
-  function Todo(title, description, dueDate, priority) {
+  function Todo(title, dueDate, priority) {
     this.title = title;
-    this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
   }
   
-  const todo1 = new Todo("Read Book", "null", "1-11-2024", "High");
-  const todo2 = new Todo("Read Book2", "null", "12-11-2024", "Low");
-  const todo3 = new Todo("Divine Speech", "Watch", "10/11/2024", "High");
+  const todo1 = new Todo("Read Book", "1-11-2024", "High");
+  const todo2 = new Todo("Read Book2", "12-11-2024", "Low");
+  const todo3 = new Todo("Divine Speech", "10/11/2024", "High");
   
   function addToDo(){
     defaultTodo.todoList.push(todo1);
